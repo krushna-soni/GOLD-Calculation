b344b84ca411ab6b4b09b81eb260cd88671b37e3
Krushna Soni <krushna.soni.ks@gmail.com>
2025-02-28 13:07:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
 const CACHE_NAME = "gold-calculator-cache-v1";
 const urlsToCache = [
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./icon-192.png",
-    "./icon-512.png"
-  ];  
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/192.png",  
+    "/512.png"   
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(urlsToCache).catch(err => {
+        console.error("Cache addAll failed:", err);
+      });
     })
   );
 });
